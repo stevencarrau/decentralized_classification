b2b5ecfb89c4967ecbe735fde9eba991438e25e0
Harish Bommakanti <harishbommakanti2002@gmail.com>
2022-02-17 19:37:20 -0600

    added HighlightReel class and implemented sorting functionality, just need to embed it in to code now

--- code/agent.py
+++ code/agent.py
@@ -34,6 +34,7 @@ class Agent:
         self.state_keys = state_keys
         self.dis = Util.prod2dis(state,states)
         self.alpha = 1.0
+        self.highlight_reel = HighlightReel(num_items=5)
 
     def likelihood(self, a, next_s, mc_dict):
         return np.array([m_i[(self.state, next_s)] for m_i in mc_dict[a]]).reshape((-1, 1))
@@ -105,3 +106,49 @@ class Agent:
         self.belief_fill.set_visible(False)
         self.belief_artist.set_visible(False)
         self.belief_text.set_visible(False)
+
+class HighlightReel:
+    """
+    Stores an array of data about the top `num_items` timesteps
+    where the change in beliefs was the highest over the episode.
+    """
+
+    # the reel is a 2D array, so this tells us what each column is mapped as
+    REEL_ITEM_LABELS_TO_IDX = {"timestep": 0, "max_delta": 1, "prev_step": 2, "next_step": 3, "trigger": 4}
+
+    def __init__(self, num_items=10):
+        self.reel_length = num_items
+        # initialize it to be full of -1's
+        self.reel = np.full((self.reel_length, len(self.REEL_ITEM_LABELS_TO_IDX)), -1)
+
+    def sort(self):
+        # sort based off of the max delta
+        column_idx = self.REEL_ITEM_LABELS_TO_IDX["max_delta"]
+        self.reel = self.reel[self.reel[:, column_idx].argsort()]
+
+    def add_item(self, reel_item):
+        empty_arr = np.full((1, len(self.REEL_ITEM_LABELS_TO_IDX)), -1)
+        empty_idxs = np.where(self.reel == empty_arr)[0]
+        # if there are still empty spots, fill it since nothing need be replaced
+        if len(empty_idxs) != 0:
+            empty_idx = empty_idxs[0]
+            self.reel[empty_idx] = reel_item.copy()
+        else:
+            # otherwise, replace item 0 as it will always be the item in
+            # the list with the smallest max delta due to sorting
+            self.reel[0] = reel_item.copy()
+
+        # sort always as a new item populates -- shouldn't be too time inefficient since only 50 entries
+        self.sort()
+
+# the following is just testing and should be deleted once highlight reel code is verified
+def main():
+    reel = HighlightReel()
+    import random
+    for t in range(15):
+        delta = random.uniform(5, 10)
+        reel.add_item(np.array([t, delta, 0, 1, 2]))
+        print(reel.reel)
+
+if __name__ == '__main__':
+    main()
